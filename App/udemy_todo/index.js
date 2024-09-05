077ca4ae55ad0e4d7993e591416f0c9ae31567e9
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-09-05 23:03:14 +0900

    🚧 完了・削除ボタンの機能追加
    完了・削除ボタンの機能追加

--- App/udemy_todo/index.js
+++ App/udemy_todo/index.js
@@ -14,17 +14,25 @@ const onClickAdd = () => {
   p.className = "todo-item"; // <p class="todo-item"></p>
   p.innerText = inputText; // <p class="todo-item">{inputText}</p>
 
-  // buttonタグ生成
+  // button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+  })
 
+  // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親にあるliタグを未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  })
 
   // liタグの子要素に各要素を設定
   div.appendChild(p); // 階層構造
-  p.appendChild(completeButton)
-  p.appendChild(deleteButton)
+  div.appendChild(completeButton)
+  div.appendChild(deleteButton)
   li.appendChild(div);
 
   // 未完成リストに追加
